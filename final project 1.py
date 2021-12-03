66be50bfd9e8d9eb6e3b287de837a2c99d3a5106
Katie Radner <katieradner@0587389863.wireless.umich.net>
2021-12-02 23:55:05 -0500

    commit

--- final project 1.py
+++ final project 1.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import ItemsView, List, Tuple
 from bs4 import BeautifulSoup
 import requests
 import unittest
@@ -45,56 +45,41 @@ def get_data_from_website(url):
         for sub_tag in tag:
             for tagg in sub_tag:
                 location_info.append(tagg)
-    print(name_info, type_info,  money_info,  review_info,  location_info)
+    return name_info, type_info,  money_info,  review_info,  location_info
 
-get_data_from_website('https://www.opentable.com/lists/top-100-2021')
-        #for sub_tag in tag:
-            #for sub_tags in sub_tag:
-                #if '\n' not in sub_tags:
-                    #collect_info.append(sub_tags)
-    #sub_list=[]
-   # count=0
-   # for i in range(len(collect_info)):
-     #   sub_list.append(collect_info[i])
-      #  count+=1
-       # if count % 3==0:
-       #     final_info_lst.append(sub_list)
-          #  sub_list=[]
-    #return final_info_lst
 
-#def setUpDatabase(db_name):
-    #path = os.path.dirname(os.path.abspath(__file__))
-    #conn = sqlite3.connect(path+'/'+db_name)
-   # cur = conn.cursor()
-    #return cur, conn
+def setUpDatabase(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    cur = conn.cursor()
+    return cur, conn
 
-#def setup_movietable(data, cur, conn):
-  #  cur.execute("CREATE TABLE movie_table (id INTEGER PRIMARY KEY, title TEXT, year TEXT)")
-  #  id=0
-   # count=0
-   ## while count <=25:
-     #   for item in data[1:]:
-      #  position=item.find('\n')
-      #  if position != -1:
-            #item=item[0:position]
-       # digit_postion=0
-       # for letter in item:
-       #     digit_postion+=1
-       ##     if letter.isdigit():
-       #         break
-       # nutrition_fact=item[0:digit_postion-2]
-       # id+=1
-       # nutrition_number=item[digit_postion-1:]
-       # if dtaa in table:
-       #     continue
-        #else:
-       # #    cur.execute("INSERT INTO dc_popcorn (id, nutrition_fact , nutrition_number ) VALUES (?,?,?)",(id, nutrition_fact , nutrition_number))
-         #   count+=1
-    #conn.commit()
+def setup_restaurantstable(data, cur, conn):
+    cur.execute("DROP TABLE IF EXISTS restaurants_table")
+    cur.execute("CREATE TABLE restaurants_table (id INTEGER PRIMARY KEY, name TEXT, type TEXT, money TEXT, review TEXT, location TEXT)")
+    id=0
+    count=0
+    #while count <=25:
+    final_list=[]
+    for item in data:
+        final_list.append(item)
+    for i in range(len(final_list)):
+        for item in range(len(final_list[i])):
+            name = final_list[0][item]
+            type=final_list[1][item]
+            money=final_list[2][item]
+            review=final_list[3][item]
+            location=final_list[4][item]
+            id+=1
+
+            cur.execute("INSERT OR IGNORE INTO restaurants_table (id, name, type, money, review, location) VALUES (?,?,?,?,?,?)",(id, name, type,money,review,location))
+
+        conn.commit()
     
+cur, conn = setUpDatabase('restaurants_database')
+web=get_data_from_website('https://www.opentable.com/lists/top-100-2021')
+setup_restaurantstable(web, cur, conn)
 
-        
 
 
-print(get_data_from_website('https://www.imdb.com/list/ls055592025/'))
       
