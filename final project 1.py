fae897e3e46c5feedbd02ca28b4d66c76c0d0b04
Katie Radner <katieradner@0587357463.wireless.umich.net>
2021-12-03 15:34:08 -0500

    commit

--- final project 1.py
+++ final project 1.py
@@ -94,14 +94,170 @@ def setup_restaurantstable(data, cur, conn):
                 cur.execute("INSERT OR IGNORE INTO restaurants_table (id, name, type, money, review, location) VALUES (?,?,?,?,?,?)",(id, name, type,money,review,location))    
         conn.commit() 
 
-# def calculate_average(column, cur, conn):
-#     cur.execute("SELECT review, money FROM restaurants_table orn ON dc_popcorn.nutrition_fact=av_popcorn.nutrition_fact JOIN ch_popcorn ON dc_popcorn.nutrition_fact=ch_popcorn.nutrition_fact JOIN gold_popcorn ON dc_popcorn.nutrition_fact=gold_popcorn.nutrition_fact JOIN sweet_popcorn ON dc_popcorn.nutrition_fact=sweet_popcorn.nutrition_fact JOIN oh_popcorn ON dc_popcorn.nutrition_fact=oh_popcorn.nutrition_fact JOIN wh_popcorn ON dc_popcorn.nutrition_fact=wh_popcorn.nutrition_fact WHERE dc_popcorn.nutrition_fact=?", (fact, ))
-#     columns=cur.fetchall()
-
+def calculate_average(cur, conn):
+    cur.execute("SELECT type, money FROM restaurants_table")
+    info=cur.fetchall()
+    type_count=0
+    money_count=0
+    for item in info:
+        if item[0]=="American":
+            type_count+=1
+            money_count+=len(item[1])
+    american_average=money_count/type_count
+    for item in info:
+        if item[0]=="Contemporary American":
+            type_count+=1
+            money_count+=len(item[1])
+    contemp_amer_average=money_count/type_count
+    for item in info:
+        if item[0]=="Italian":
+            type_count+=1
+            money_count+=len(item[1])
+    italian_average=money_count/type_count
+    for item in info:
+        if item[0]=="Afghan":
+            type_count+=1
+            money_count+=len(item[1])
+    afghan_average=money_count/type_count
+    for item in info:
+        if item[0]=="French":
+            type_count+=1
+            money_count+=len(item[1])
+    french_average=money_count/type_count
+    for item in info:
+        if item[0]=="Southwest":
+            type_count+=1
+            money_count+=len(item[1])
+    southwest_average=money_count/type_count
+    for item in info:
+        if item[0]=="Contemporary French":
+            type_count+=1
+            money_count+=len(item[1])
+    contemp_french_average=money_count/type_count
+    for item in info:
+        if item[0]=="Tapas / Small Plates":
+            type_count+=1
+            money_count+=len(item[1])
+    tapas_average=money_count/type_count
+    for item in info:
+        if item[0]=="French American":
+            type_count+=1
+            money_count+=len(item[1])
+    french_amer_average=money_count/type_count
+    for item in info:
+        if item[0]=="Seafood":
+            type_count+=1
+            money_count+=len(item[1])
+    sea_average=money_count/type_count
+    for item in info:
+        if item[0]=="Winery":
+            type_count+=1
+            money_count+=len(item[1])
+    winery_average=money_count/type_count
+    for item in info:
+        if item[0]=="Steakhouse":
+            type_count+=1
+            money_count+=len(item[1])
+    steakhouse_average=money_count/type_count
+    for item in info:
+        if item[0]=="Greek":
+            type_count+=1
+            money_count+=len(item[1])
+    greek_average=money_count/type_count
+    for item in info:
+        if item[0]=="Fusion / Eclectic":
+            type_count+=1
+            money_count+=len(item[1])
+    fusion_average=money_count/type_count
+    for item in info:
+        if item[0]=="Mediterranean":
+            type_count+=1
+            money_count+=len(item[1])
+    medit_average=money_count/type_count
+    for item in info:
+        if item[0]=="Vietnamese":
+            type_count+=1
+            money_count+=len(item[1])
+    viet_average=money_count/type_count
+    for item in info:
+        if item[0]=="Peruvian":
+            type_count+=1
+            money_count+=len(item[1])
+    peru_average=money_count/type_count
+    for item in info:
+        if item[0]=="Mexican":
+            type_count+=1
+            money_count+=len(item[1])
+    mexican_average=money_count/type_count
+    for item in info:
+        if item[0]=="Steak":
+            type_count+=1
+            money_count+=len(item[1])
+    steak_average=money_count/type_count
+    for item in info:
+        if item[0]=="Sushi":
+            type_count+=1
+            money_count+=len(item[1])
+    sushi_average=money_count/type_count
+    for item in info:
+        if item[0]=="Fish":
+            type_count+=1
+            money_count+=len(item[1])
+    fish_average=money_count/type_count
+    for item in info:
+        if item[0]=="Traditional French":
+            type_count+=1
+            money_count+=len(item[1])
+    trad_french_average=money_count/type_count
+    for item in info:
+        if item[0]=="Farm-to-table":
+            type_count+=1
+            money_count+=len(item[1])
+    farm_average=money_count/type_count
+    for item in info:
+        if item[0]=="Croatian":
+            type_count+=1
+            money_count+=len(item[1])
+    croatian_average=money_count/type_count
+    for item in info:
+        if item[0]=="Speakeasy":
+            type_count+=1
+            money_count+=len(item[1])
+    speak_average=money_count/type_count
     
+    final_str=american_average, italian_average, contemp_amer_average, afghan_average, french_average, southwest_average, contemp_french_average, tapas_average, french_amer_average, sea_average, winery_average, steakhouse_average, greek_average, fusion_average, steak_average, medit_average, viet_average, peru_average, mexican_average, sushi_average, fish_average, trad_french_average, farm_average, croatian_average, speak_average
+    final_list=[]
+    final_list.append(final_str)
+
+    with open('restaurants.txt', 'w') as f:
+        for item in info:
+            for i in range(len(final_list)):
+               f.writelines('The average cost of ' + str(item[0][i]) + 'type of restaurant is $' + str(final_list[i]) + ".")
+               f.write('\n')
+    f.close()
+
+    # type_dict={}
+    # money_dict={}
+    # final_dict={}
+    # count_money=0
+    # for item in info:
+    #     if item[0] not in type_dict:
+    #         type_dict[item[0]]=0
+    #     type_dict[item[0]]+=1
+    #     if item[0]==item[0]:
+    #         dollar=len(item[1])
+    #         count_money+=dollar
+    #         money_dict[item[0]]=count_money
+    #         #print(count_money)
+    #     #final_dict[item[0]]=count_money/type_dict[item][1]
+    # #print(final_dict)
+
+
 cur, conn = setUpDatabase('restaurants_database')
 web=get_data_from_website('https://www.opentable.com/lists/top-100-2021')
 setup_restaurantstable(web, cur, conn)
+calculate_average(cur,conn)
+
 
 
 
