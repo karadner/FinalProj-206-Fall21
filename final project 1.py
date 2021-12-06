3b163f232a214c9094aad8bc1f408975f8d0585e
Katie Radner <katieradner@Katies-Air-2.lan>
2021-12-06 09:54:15 -0500

    commit

--- final project 1.py
+++ final project 1.py
@@ -9,8 +9,13 @@ import matplotlib
 import matplotlib.pyplot as plt
 import random
 
-#restaurantname, typefood, money symbol, reviews, location
+
 def get_data_from_website(url):
+    """
+    This function scrapes the data from the Top100 Diners website using 
+    BeautifulSoup. It returns multiple lists of the names, the type, 
+    the dollar signs (cost), the reviews, and the location for each restaurant. 
+    """
     req = requests.get(url)
     soup = BeautifulSoup(req.content, 'html.parser')
     name_tags = soup.find_all('h2')
@@ -50,15 +55,30 @@ def get_data_from_website(url):
 
 
 def setUpDatabase(db_name):
+    """
+    This function creates the database titled "restaurants_database". 
+    It returns the connection to the database and the cursor.
+    """
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path+'/'+db_name)
     cur = conn.cursor()
     return cur, conn
 
 def create_table(cur,conn):
-    cur.execute("DROP TABLE IF EXISTS restaurants_table")
+    """
+    This function creates the restaurants table in the databse.
+    It takes 2 arguments such as the database cursor, and database connection 
+    object. It does not return anything. 
+    """
     cur.execute("CREATE TABLE restaurants_table (id INTEGER PRIMARY KEY, name TEXT, type TEXT, money TEXT, review TEXT, location TEXT)")
 def setup_restaurantstable(data, cur, conn):
+    """
+    This function puts the data into the restaurants_table. 
+    It takes in 3 arguments such as data, which is the url of the website, 
+    the database cursor, and the database connection object. 
+    The table has 6 columns. They are id, restaurant name, type of restaurant,
+    cost of restaurant (in $'s), number of reviews, and location.
+    """
     id=0
     count=0
     final_list=[]
@@ -78,7 +98,7 @@ def setup_restaurantstable(data, cur, conn):
         id=max(id_list)
     except:
         id=0
-        create_table(cur,conn)
+        create_table(cur, conn)
     while count <25:
         if id>=100:
             break
@@ -93,170 +113,64 @@ def setup_restaurantstable(data, cur, conn):
                 count+=1
                 cur.execute("INSERT OR IGNORE INTO restaurants_table (id, name, type, money, review, location) VALUES (?,?,?,?,?,?)",(id, name, type,money,review,location))    
         conn.commit() 
-
 def calculate_average(cur, conn):
+    """
+    This function calculates the average number of dollar signs for each type of 
+    restaurant. It takes in 2 arguments such as the database cursor and 
+    the database connection object. It then wrties these averages in the 
+    restaurant_calculations.txt file. The last section of this function creates 
+    a visual using matplotlib to create a bar graph of these adverages. 
+    """
     cur.execute("SELECT type, money FROM restaurants_table")
     info=cur.fetchall()
-    type_count=0
-    money_count=0
+    count_dict={}
+    total_dict={}
+    average_dict={}
     for item in info:
-        if item[0]=="American":
-            type_count+=1
-            money_count+=len(item[1])
-    american_average=money_count/type_count
-    for item in info:
-        if item[0]=="Contemporary American":
-            type_count+=1
-            money_count+=len(item[1])
-    contemp_amer_average=money_count/type_count
-    for item in info:
-        if item[0]=="Italian":
-            type_count+=1
-            money_count+=len(item[1])
-    italian_average=money_count/type_count
-    for item in info:
-        if item[0]=="Afghan":
-            type_count+=1
-            money_count+=len(item[1])
-    afghan_average=money_count/type_count
-    for item in info:
-        if item[0]=="French":
-            type_count+=1
-            money_count+=len(item[1])
-    french_average=money_count/type_count
-    for item in info:
-        if item[0]=="Southwest":
-            type_count+=1
-            money_count+=len(item[1])
-    southwest_average=money_count/type_count
-    for item in info:
-        if item[0]=="Contemporary French":
-            type_count+=1
-            money_count+=len(item[1])
-    contemp_french_average=money_count/type_count
-    for item in info:
-        if item[0]=="Tapas / Small Plates":
-            type_count+=1
-            money_count+=len(item[1])
-    tapas_average=money_count/type_count
-    for item in info:
-        if item[0]=="French American":
-            type_count+=1
-            money_count+=len(item[1])
-    french_amer_average=money_count/type_count
-    for item in info:
-        if item[0]=="Seafood":
-            type_count+=1
-            money_count+=len(item[1])
-    sea_average=money_count/type_count
-    for item in info:
-        if item[0]=="Winery":
-            type_count+=1
-            money_count+=len(item[1])
-    winery_average=money_count/type_count
-    for item in info:
-        if item[0]=="Steakhouse":
-            type_count+=1
-            money_count+=len(item[1])
-    steakhouse_average=money_count/type_count
-    for item in info:
-        if item[0]=="Greek":
-            type_count+=1
-            money_count+=len(item[1])
-    greek_average=money_count/type_count
-    for item in info:
-        if item[0]=="Fusion / Eclectic":
-            type_count+=1
-            money_count+=len(item[1])
-    fusion_average=money_count/type_count
-    for item in info:
-        if item[0]=="Mediterranean":
-            type_count+=1
-            money_count+=len(item[1])
-    medit_average=money_count/type_count
-    for item in info:
-        if item[0]=="Vietnamese":
-            type_count+=1
-            money_count+=len(item[1])
-    viet_average=money_count/type_count
-    for item in info:
-        if item[0]=="Peruvian":
-            type_count+=1
-            money_count+=len(item[1])
-    peru_average=money_count/type_count
-    for item in info:
-        if item[0]=="Mexican":
-            type_count+=1
-            money_count+=len(item[1])
-    mexican_average=money_count/type_count
-    for item in info:
-        if item[0]=="Steak":
-            type_count+=1
-            money_count+=len(item[1])
-    steak_average=money_count/type_count
-    for item in info:
-        if item[0]=="Sushi":
-            type_count+=1
-            money_count+=len(item[1])
-    sushi_average=money_count/type_count
-    for item in info:
-        if item[0]=="Fish":
-            type_count+=1
-            money_count+=len(item[1])
-    fish_average=money_count/type_count
-    for item in info:
-        if item[0]=="Traditional French":
-            type_count+=1
-            money_count+=len(item[1])
-    trad_french_average=money_count/type_count
-    for item in info:
-        if item[0]=="Farm-to-table":
-            type_count+=1
-            money_count+=len(item[1])
-    farm_average=money_count/type_count
-    for item in info:
-        if item[0]=="Croatian":
-            type_count+=1
-            money_count+=len(item[1])
-    croatian_average=money_count/type_count
-    for item in info:
-        if item[0]=="Speakeasy":
-            type_count+=1
-            money_count+=len(item[1])
-    speak_average=money_count/type_count
-    
-    final_str=american_average, italian_average, contemp_amer_average, afghan_average, french_average, southwest_average, contemp_french_average, tapas_average, french_amer_average, sea_average, winery_average, steakhouse_average, greek_average, fusion_average, steak_average, medit_average, viet_average, peru_average, mexican_average, sushi_average, fish_average, trad_french_average, farm_average, croatian_average, speak_average
-    final_list=[]
-    final_list.append(final_str)
-
-    with open('restaurants.txt', 'w') as f:
-        for item in info:
-            for i in range(len(final_list)):
-               f.writelines('The average cost of ' + str(item[0][i]) + 'type of restaurant is $' + str(final_list[i]) + ".")
-               f.write('\n')
+        if item[0] not in count_dict:
+            count_dict[item[0]]=1
+        else:
+            count_dict[item[0]]+=1
+        if item[0] not in total_dict:
+            total_dict[item[0]]=len(item[1])
+        else:
+            total_dict[item[0]]+=len(item[1])
+    for type in count_dict:
+        average_dict[type]=round(total_dict[type]/count_dict[type],2)
+       
+    with open('restaurant_calculations.txt', 'w') as f:
+        for type in average_dict:
+            if str(type).startswith('A') or str(type).startswith('E') or str(type).startswith('I') or str(type).startswith('O') or str(type).startswith('U') or str(type).startswith('Y'):
+                f.writelines('The average cost of an ' + type + ' restaurant is ' + str(average_dict[type]) + " $'s.")
+                f.write('\n')
+            else:
+                f.writelines('The average cost of a ' + type + ' restaurant is ' + str(average_dict[type]) + " $'s.")
+                f.write('\n')
     f.close()
 
-    # type_dict={}
-    # money_dict={}
-    # final_dict={}
-    # count_money=0
-    # for item in info:
-    #     if item[0] not in type_dict:
-    #         type_dict[item[0]]=0
-    #     type_dict[item[0]]+=1
-    #     if item[0]==item[0]:
-    #         dollar=len(item[1])
-    #         count_money+=dollar
-    #         money_dict[item[0]]=count_money
-    #         #print(count_money)
-    #     #final_dict[item[0]]=count_money/type_dict[item][1]
-    # #print(final_dict)
 
+    fig=plt.figure(figsize=(16,8))
+    matplotlib.rc('xtick', labelsize=5) 
+    matplotlib.rc('ytick', labelsize=5)
+    averages_list=[]
+    names_list=[]
+    for type in average_dict:
+        names_list.append(type)
+        averages_list.append(average_dict[type])
+    plt.bar(names_list, averages_list, color=['midnightblue', 'mediumblue', 'blue', 'indigo', 'darkslateblue', 'royalblue', 'steelblue', 'dodgerblue', 'deepskyblue', 'lightskyblue', 'slateblue', 'mediumslateblue', 'purple', 'rebeccapurple', 'darkviolet', 'blueviolet', 'darkorchid', 'mediumpurple', 'mediumorchid', 'plum', 'violet', 'orchid', 'hotpink', 'lightpink', 'coral'], edgecolor='black')
+    plt.xlabel('Type of Restaurant', fontweight='bold')
+    plt.ylabel("Average $'s", fontweight='bold')
+    plt.title("Average $'s for Type of Restaurant")
+    plt.xticks(rotation=45)
+    plt.show()
 
-cur, conn = setUpDatabase('restaurants_database')
-web=get_data_from_website('https://www.opentable.com/lists/top-100-2021')
-setup_restaurantstable(web, cur, conn)
-calculate_average(cur,conn)
+def main():
+    cur, conn = setUpDatabase('restaurants_database')
+    web=get_data_from_website('https://www.opentable.com/lists/top-100-2021')
+    setup_restaurantstable(web, cur, conn)
+    calculate_average(cur,conn)
+main()
+
 
 
 
